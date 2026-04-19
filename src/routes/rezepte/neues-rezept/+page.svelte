15ad948baedacf650cbe7780dceebec32f2631e4
Dave <davidan631@gmail.com>
2026-04-19 13:00:11 +0200

    self closing tags because thhe input stuff does not have seperate tag/do not wrap around content

--- src/routes/rezepte/neues-rezept/+page.svelte
+++ src/routes/rezepte/neues-rezept/+page.svelte
@@ -1,7 +1,7 @@
 <form method="POST">
-    <input type="text" name="titel">
-    <input type="number" name="portionen">
-    <input type="number" name="zubereitungszeit">
-    <textarea name="anleitung"></textarea>
-    <button type="submit">Speichern</button>
+	<input type="text" name="titel" />
+	<input type="number" name="portionen" />
+	<input type="number" name="zubereitungszeit" />
+	<textarea name="anleitung"></textarea>
+	<button type="submit">Speichern</button>
 </form>
