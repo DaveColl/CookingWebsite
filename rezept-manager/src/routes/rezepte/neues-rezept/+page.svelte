88b773d04ed83f6a7cbd76d0f00c28f25be16900
Dave <davidan631@gmail.com>
2026-04-19 12:49:28 +0200

    moved folder structure to be correct

--- rezept-manager/src/routes/rezepte/neues-rezept/+page.svelte
+++
@@ -1,4 +0,0 @@
-<input type="text" name="titel">
-<input type="number" name="portionen">
-<textarea name="anleitung"></textarea>
-<button type="submit">Speichern</button>
---
+++ src/routes/rezepte/neues-rezept/+page.svelte
@@ -0,0 +1,7 @@
+<form method="POST">
+    <input type="text" name="titel">
+    <input type="number" name="portionen">
+    <input type="number" name="zubereitungszeit">
+    <textarea name="anleitung"></textarea>
+    <button type="submit">Speichern</button>
+</form>
