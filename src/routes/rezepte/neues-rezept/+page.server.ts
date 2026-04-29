6ef649972bb189b70909d0985855ef166a1cac23
Dave <davidan631@gmail.com>
2026-04-30 00:09:39 +0200

    database import commented out

--- src/routes/rezepte/neues-rezept/+page.server.ts
+++ src/routes/rezepte/neues-rezept/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions } from './$types';
-import Database from 'better-sqlite3';
+//import Database from 'better-sqlite3';
 
 // Connect to your database
 //const db = new Database('rezepte.db');
